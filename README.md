e206e254d87a3461630e3e3a134cbd0cff8bb8fa
Jayanth Parayil Kumarji <jp972@cornell.edu>
2015-09-13 16:00:07 -0700

    Upsert & search call added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # anyOneAtHome
 Detects presence of devices in a wifi network
+
+
+
+create table userInfo (
+        mac varchar(30) primary key,
+        name varchar(50),
+        alias varchar(30);
+        deviceType varchar(20),
+        lastseen timestamp
+);
+ create index mac_index on userinfo (mac);
+ insert into userinfo(name,devicetype,mac,lastseen) values('lol','iphone','MMM',transaction_timestamp());
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
+
+var lastSeenInterval = 30;
+var conString = process.env.DATABASE_URL;
+var findQueryString = 'select * from userinfo where lastseen > (current_timestamp - interval \''+ lastSeenInterval+'\' minute)';
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,8 +18,104 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+//web function
+app.get('/check', function(request, response) {
+      pg.connect(conString, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query('select * from userinfo where lastseen > (current_timestamp - interval \''+lastSeenInterval+'\' minute)',function(err, result) {
+        done();        //call `done()` to release the client back to the pool
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows);
+        return response.json(result.rows);
+      });
+    });
+    // response.render('pages/index');
+});
+
+//web function
+app.post('/register', function(request, response) {
+      pg.connect(conString, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        // insert into userinfo(name,devicetype,mac,lastseen) values('lol','iphone','MMM',transaction_timestamp());
+
+        client.query('insert into userInfo(name,devicetype,mac,lastseen) values($1,$2,$3,transaction_timestamp())',['max','iphone','SSccaass'],function(err, result) {
+        done();        //call `done()` to release the client back to the pool
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows);
+        return response.json(result);
+      });
+    });
+    // response.render('pages/index');
+});
+
+
+//client function
+app.post('/update', function(request, response) {
+      var jsonString ='';
+      var userInfo = new Array();
+      var property ='';
+      request.on('data', function (data) {
+           jsonString = data.toString().split('&');
+           console.log(jsonString);
+
+           for(i=0;i<jsonString.length;i++){
+              property = jsonString[i].toString().split('=');
+
+              if(property.length >1){
+                userInfo[property[0]]=property[1];
+              }
+           }
+           if(userInfo.mac){
+              upsert(userInfo);
+           }
+           console.log(userInfo);
+       });
+
+    return response.json("status : success");
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 
+function upsert(userInfo){
+      pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          client.query('update userinfo set lastseen = now() where mac = $1',[userInfo.mac],function(err, result) {
+          userPresent = result.rowCount;
+
+          if(err) {
+            return console.error('error running query', err);
+          }
+          console.log("Update rowcount : "+ result.rowCount);
+
+          if(userPresent == 0){
+                console.log(userInfo);
+                userName = "user_"+Math.floor(Math.random()*10000);
+                client.query('insert into userInfo(name,alias,devicetype,mac,lastseen) values($1,$2,$3,$4,transaction_timestamp())',[userName,userName,'unkown device',userInfo.mac],function(err, insertResult) {
+
+                if(err) {
+                  return console.error('error running query', err);
+                }
+                console.log("Inserted rowcount : "+insertResult.rowCount);
+              });
+          }
+          done();
+        });
+    });
+}
